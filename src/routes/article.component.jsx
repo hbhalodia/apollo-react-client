73ae6517bb0bb10a56a6e09c52b350b6e7ec4a25
hbhalodia <hit.bhalodia@rtcamp.com>
2023-03-28 14:45:03 +0530

    Add article counter logic and update meta using mutations

--- src/routes/article.component.jsx
+++ src/routes/article.component.jsx
@@ -1,7 +1,7 @@
-import { Fragment, useEffect } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { Outlet, useParams } from 'react-router-dom';
 
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql, useMutation } from '@apollo/client';
 
 const GET_SINGLE_ARTICLE = gql`
 	query GetArticle($id: ID!, $postType: String) {
@@ -10,12 +10,25 @@ const GET_SINGLE_ARTICLE = gql`
 			title
 			content
 			excerpt
+			meta {
+				metaKey
+				metaValue
+			}
 			attachment {
 				source_url
 			}
 			author {
 				name
 			}
+		}
+	}
+`;
+
+const SINGLE_ARTICLE_COUNTER = gql`
+	mutation ArticleCounter($postId: ID!, $postType: String, $key: String, $value: String) {
+		addArticleViewCount(postId: $postId, postType: $postType, metaKey: $key, metaValue: $value) {
+			id
+			title
 		}
 	}
 `;
@@ -27,23 +40,48 @@ const Article = () => {
 		document.title = 'Article: ' + slug;
 	}, [slug]);
 
-	const { loading, error, data } = useQuery(GET_SINGLE_ARTICLE, {
+	const [isArticleViewed, setIsArticleViewed] = useState(false);
+
+	const { loading: queryLoading, error: queryError, data: queryData, refetch: queryRefetch } = useQuery(GET_SINGLE_ARTICLE, {
 		variables: { id: id, postType: 'article' },
 	});
 
-	if (loading) return <p>Loading...</p>;
-	if (error) return <p>Error : {error.message}</p>;
+	const [addArticleCounter, { loading: mutateLoading, error: mutateError }] = useMutation(SINGLE_ARTICLE_COUNTER);
+
+	if (queryLoading) return <p>Loading...</p>;
+	if (queryError) return <p>Error : {queryError.message}</p>;
+
+	if (mutateLoading) return <p>Adding Article Count...</p>;
+	if (mutateError) return <p>Submission error! {mutateError.message}</p>;
+
+	let keyName = null;
+	let keyValue = null;
+
+	if (queryData.article.meta) {
+		queryData.article.meta.forEach((meta) => {
+			if (meta.metaKey === 'article-view-count') {
+				keyName = meta.metaKey;
+				keyValue = meta.metaValue;
+			}
+		});
+	}
+
+	if (!isArticleViewed) {
+		setIsArticleViewed(true);
+		queryRefetch();
+		addArticleCounter({ variables: { postId: id, postType: 'article', key: keyName, value: String(parseInt(keyValue) + 1) } });
+	}
 
 	return (
 		<Fragment>
 			<div className='article-container'>
-				<div className='article-container__title' dangerouslySetInnerHTML={{ __html: data.article.title }}></div>
-				<div className='article-container__author' dangerouslySetInnerHTML={{ __html: `By ` + data.article.author.name }}></div>
-				<div className='article-container__excerpt' dangerouslySetInnerHTML={{ __html: data.article.excerpt }}></div>
+				<div className='article-container__title' dangerouslySetInnerHTML={{ __html: queryData.article.title }}></div>
+				<div className='article-container__author' dangerouslySetInnerHTML={{ __html: `By ` + queryData.article.author.name }}></div>
+				<div className='article-container__excerpt' dangerouslySetInnerHTML={{ __html: queryData.article.excerpt }}></div>
 				<div className='article-container__image'>
-					<img height="500" width="500" src={data.article.attachment.source_url} alt={data.article.title} />
+					<img height="500" width="500" src={queryData.article.attachment.source_url} alt={queryData.article.title} />
 				</div>
-				<div className='article-container__content' dangerouslySetInnerHTML={{ __html: data.article.content }} ></div>
+				<div className='article-container__content' dangerouslySetInnerHTML={{ __html: queryData.article.content }} ></div>
 			</div>
 			<Outlet />
 		</Fragment>
