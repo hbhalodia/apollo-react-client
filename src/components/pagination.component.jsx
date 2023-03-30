eadfd70b5090cb122886bc73f0d358cd23db0489
hbhalodia <hit.bhalodia@rtcamp.com>
2023-03-30 16:19:07 +0530

    Add axios to fetch the data instead of useQuery

---
+++ src/components/pagination.component.jsx
@@ -0,0 +1,73 @@
+const Pagination = ({ totalPages, onPageChange, currentPage }) => {
+
+	const handlePageClick = (pageNumber) => {
+		onPageChange(pageNumber);
+	};
+
+	const handlePrevClick = () => {
+		if (currentPage > 1) {
+			onPageChange(currentPage - 1);
+		}
+	};
+
+	const handleNextClick = () => {
+		if (currentPage < totalPages) {
+			onPageChange(currentPage + 1);
+		}
+	};
+
+	const renderPageNumbers = () => {
+		const pageNumbers = [];
+
+		pageNumbers.push(
+			<li key={1} className={currentPage === 1 ? 'active' : null}>
+				<button className='pagination-number' onClick={() => handlePageClick(1)}>{1}</button>
+			</li>
+		);
+
+		let ellipses = false;
+		for (let i = 2; i <= totalPages - 1; i++) {
+			if (i === currentPage - 1 || i === currentPage || i === currentPage + 1) {
+				// Show the current page and the pages immediately before and after it
+				pageNumbers.push(
+					<li key={i} className={currentPage === i ? 'active' : null}>
+						<button className='pagination-number' onClick={() => handlePageClick(i)}>{i}</button>
+					</li>
+				);
+				ellipses = false;
+			} else if (!ellipses) {
+				pageNumbers.push(
+					<li key={`ellipses${i}`} className="disabled">
+						<span>...</span>
+					</li>
+				);
+				ellipses = true;
+			}
+		}
+
+		if (totalPages > 1) {
+			pageNumbers.push(
+				<li key={totalPages} className={currentPage === totalPages ? 'active' : null}>
+					<button className='pagination-number' onClick={() => handlePageClick(totalPages)}>{totalPages}</button>
+				</li>
+			);
+		}
+		return pageNumbers;
+	};
+
+	return (
+		<div className="pagination-container">
+			<ul className="pagination-list">
+				<li className={currentPage === 1 ? 'disabled' : null}>
+					<button className='pagination-number' onClick={handlePrevClick}>Prev</button>
+				</li>
+				{renderPageNumbers()}
+				<li className={currentPage === totalPages ? 'disabled' : null}>
+					<button className='pagination-number' onClick={handleNextClick}>Next</button>
+				</li>
+			</ul>
+		</div>
+	);
+}
+
+export default Pagination;
--- src/routes/home.component.jsx
+++ src/routes/home.component.jsx
@@ -1,9 +1,11 @@
 import { Outlet, Link, useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
-import { useQuery, gql } from '@apollo/client';
+import axios from 'axios';
 
-export const GET_HOME_ARTICLES = gql`
+import Pagination from '../components/pagination.component';
+
+export const GET_HOME_ARTICLES = `
 	query GetHomeArticles($pageSize: Int, $postType: String, $page: Int) {
 		articles(pageSize: $pageSize, postType: $postType, page: $page) {
 			id
@@ -21,8 +23,10 @@ const Home = () => {
 
 	const pageSize = 10;
 	const location = useLocation();
-
 	const [currentPage, setCurrentPage] = useState(1);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+	const [totalPages, setTotalPages] = useState(0);
 
 	useEffect(() => {
 		const storedPage = parseInt(localStorage.getItem('currentPage' + location.pathname), 10);
@@ -34,46 +38,45 @@ const Home = () => {
 		}
 	}, [location.pathname]);
 
-
-	const { loading, error, data, fetchMore } = useQuery(GET_HOME_ARTICLES, {
-		variables: { postType: 'article', pageSize: pageSize, page: currentPage },
-	});
+	const [data, setData] = useState([]);
 
 	useEffect(() => {
-		document.title = 'HomePage - Latest News, Articles - ' + currentPage;
+
+		document.title = 'HomePage - Latest News, Articles - Page ' + currentPage;
+
+		setLoading(true); // Make loading true.
+
+		axios.post('http://localhost:4002/', {
+			query: GET_HOME_ARTICLES,
+			variables: {
+				postType: 'article',
+				pageSize: pageSize,
+				page: currentPage
+			},
+		}).then(response => {
+
+			console.log(response.headers['x-wp-totalpages']);
+			if (response.data.data.articles) {
+				setData(response.data.data.articles);
+				setLoading(false);
+			}
+
+			if (response.headers['x-wp-totalpages']) {
+				setTotalPages(response.headers['x-wp-totalpages']);
+			}
+		}).catch(error => {
+			console.error(error);
+			setLoading(false);
+			setError(true);
+		});
 	}, [currentPage]);
 
 	if (loading) return <p>Loading...</p>;
-	if (error) return <p>Error : {error.message}</p>;
+	if (error) return <p>Error</p>;
 
-	const handleLoadMore = () => {
-		setCurrentPage(currentPage + 1);
-		fetchMore({
-			variables: { page: currentPage + 1 },
-		});
-		localStorage.setItem('currentPage' + location.pathname , currentPage + 1);
-	}
-
-	const handlePreviousPage = () => {
-
-		if (currentPage === 1) {
-			setCurrentPage(1);
-		} else {
-			setCurrentPage(currentPage - 1);
-		}
-
-		fetchMore({
-			variables: { page: currentPage - 1 },
-		});
-		localStorage.setItem('currentPage' + location.pathname, currentPage - 1);
-	}
-
-	const clearPagination = () => {
-		setCurrentPage(1);
-		fetchMore({
-			variables: { page: 1 },
-		});
-		localStorage.setItem('currentPage' + location.pathname, 1);
+	const onPageChange = (pageNumber) => {
+		localStorage.setItem('currentPage' + location.pathname, pageNumber);
+		setCurrentPage(pageNumber);
 	}
 
 	return (
@@ -83,7 +86,7 @@ const Home = () => {
 			</div>
 			<div className='homepage-articles-container'>
 				{
-					data.articles.map((article) => (
+					data.map((article) => (
 						<div className='homepage-article' key={article.id}>
 							<Link className='article-link' to={`/article/${article.slug}/${article.id}`}>
 								{
@@ -99,11 +102,7 @@ const Home = () => {
 					))
 				}
 			</div>
-			<div className="pagination-buttons">
-				<button onClick={handlePreviousPage}>Previous - {currentPage-1}</button>
-				<button onClick={clearPagination}>Clear</button>
-				<button onClick={handleLoadMore}>Next - {currentPage + 1}</button>
-			</div>
+			<Pagination totalPages={totalPages} onPageChange={onPageChange} currentPage={currentPage} />
 			<Outlet />
 		</>
 	);
