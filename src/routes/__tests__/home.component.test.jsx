b73cd904ee3b716f35836ec4e97a4f876f9af404
hbhalodia <hit.bhalodia@rtcamp.com>
2023-03-31 11:01:18 +0530

    Add unit test for axios method

--- src/routes/__tests__/home.component.test.jsx
+++ src/routes/__tests__/home.component.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
 import "@testing-library/jest-dom";
 
@@ -7,83 +7,170 @@ import Home from '../home.component';
 
 import { MemoryRouter } from 'react-router-dom';
 
-describe('Home Article List Component', () => {
-	it('should render loading state initially', async () => {
-		const mocks = [
-			{
-				request: {
-					query: GET_HOME_ARTICLES,
-					variables: {
-						pageSize: 10,
-						postType: 'article',
-						page: 1,
-					},
-				},
-				result: {
-					data: {
-						articles: [
-							{
-								id: '128',
-								title: 'testing',
-								slug: 'testing',
-								excerpt: 'Excerpt',
-								attachment: {
-									source_url: 'https://www.example.com/image.jpg',
-								}
+// describe('Home Article List Component', () => {
+// 	it('should render loading state initially', async () => {
+// 		const mocks = [
+// 			{
+// 				request: {
+// 					query: GET_HOME_ARTICLES,
+// 					variables: {
+// 						pageSize: 10,
+// 						postType: 'article',
+// 						page: 1,
+// 					},
+// 				},
+// 				result: {
+// 					data: {
+// 						articles: [
+// 							{
+// 								id: '128',
+// 								title: 'testing',
+// 								slug: 'testing',
+// 								excerpt: 'Excerpt',
+// 								attachment: {
+// 									source_url: 'https://www.example.com/image.jpg',
+// 								}
+// 							}
+// 						]
+// 					},
+// 				},
+// 			},
+// 		];
+
+// 		render(
+// 			<MemoryRouter>
+// 				<MockedProvider mocks={mocks} addTypename={false}>
+// 					<Home />
+// 				</MockedProvider>
+// 			</MemoryRouter>
+// 		);
+
+// 		expect(await screen.findByText("Loading...")).toBeInTheDocument();
+// 	});
+
+// 	it('should render data after query is complete', async () => {
+// 		const mocks = [
+// 			{
+// 				request: {
+// 					query: GET_HOME_ARTICLES,
+// 					variables: {
+// 						pageSize: 10,
+// 						postType: 'article',
+// 						page: 1,
+// 					},
+// 				},
+// 				result: {
+// 					data: {
+// 						articles: [
+// 							{
+// 								id: '128',
+// 								title: 'testing',
+// 								slug: 'testing',
+// 								excerpt: 'Excerpt',
+// 								attachment: {
+// 									source_url: 'https://www.example.com/image.jpg',
+// 								}
+// 							}
+// 						]
+// 					},
+// 				},
+// 			},
+// 		];
+
+// 		render(
+// 			<MemoryRouter>
+// 				<MockedProvider mocks={mocks} addTypename={false}>
+// 					<Home />
+// 				</MockedProvider>
+// 			</MemoryRouter>
+// 		);
+
+// 		await screen.findByText('testing');
+
+// 		const dataElement = screen.getByText('testing');
+
+// 		expect(dataElement).toBeInTheDocument();
+// 	});
+
+// 	it('should render error state if query fails', async () => {
+// 		const mocks = [
+// 			{
+// 				request: {
+// 					query: GET_HOME_ARTICLES,
+// 					variables: {
+// 						pageSize: 10,
+// 						postType: 'article',
+// 						page: 1,
+// 					},
+// 				},
+// 				error: new Error('Failed to fetch data'),
+// 			},
+// 		];
+
+// 		render(
+// 			<MemoryRouter>
+// 				<MockedProvider mocks={mocks} addTypename={false}>
+// 					<Home />
+// 				</MockedProvider>
+// 			</MemoryRouter>
+// 		);
+
+// 		const errorElement = await screen.findByText('Error : Failed to fetch data');
+
+// 		expect(errorElement).toBeInTheDocument();
+// 	});
+// });
+
+import axios from 'axios';
+
+jest.mock('axios');
+
+describe('Home Component', () => {
+	it('renders data', async () => {
+
+		const mockResponse = {
+			data: {
+				data: {
+					articles: [
+						{
+							id: '128',
+							title: 'testing',
+							excerpt: 'testing excerpt',
+							slug: 'testng slug',
+							attachment: {
+								source_url: 'https://example.com/imag.jpg',
 							}
-						]
-					},
-				},
+						}
+					]
+				}
 			},
-		];
+			status: 200,
+			headers: {
+				'x-wp-totalpages': 17,
+			},
+			config: {}
+		}
+
+		axios.post.mockResolvedValueOnce(mockResponse);
 
 		render(
 			<MemoryRouter>
-				<MockedProvider mocks={mocks} addTypename={false}>
-					<Home />
-				</MockedProvider>
+				<Home />
 			</MemoryRouter>
 		);
 
-		expect(await screen.findByText("Loading...")).toBeInTheDocument();
-	});
+		await waitFor(() => expect(axios.post).toHaveBeenCalledTimes(1));
 
-	it('should render data after query is complete', async () => {
-		const mocks = [
-			{
-				request: {
-					query: GET_HOME_ARTICLES,
-					variables: {
-						pageSize: 10,
-						postType: 'article',
-						page: 1,
-					},
-				},
-				result: {
-					data: {
-						articles: [
-							{
-								id: '128',
-								title: 'testing',
-								slug: 'testing',
-								excerpt: 'Excerpt',
-								attachment: {
-									source_url: 'https://www.example.com/image.jpg',
-								}
-							}
-						]
-					},
-				},
+		await axios.post('http://localhost:4002/', {
+			query: GET_HOME_ARTICLES,
+			variables: {
+				postType: 'article',
+				pageSize: 10,
+				page: parseInt(2)
 			},
-		];
+		});
 
-		render(
-			<MemoryRouter>
-				<MockedProvider mocks={mocks} addTypename={false}>
-					<Home />
-				</MockedProvider>
-			</MemoryRouter>
-		);
+		await waitFor(() => expect(axios.post).toHaveBeenCalledTimes(2));
 
 		await screen.findByText('testing');
 
@@ -91,32 +178,4 @@ describe('Home Article List Component', () => {
 
 		expect(dataElement).toBeInTheDocument();
 	});
-
-	it('should render error state if query fails', async () => {
-		const mocks = [
-			{
-				request: {
-					query: GET_HOME_ARTICLES,
-					variables: {
-						pageSize: 10,
-						postType: 'article',
-						page: 1,
-					},
-				},
-				error: new Error('Failed to fetch data'),
-			},
-		];
-
-		render(
-			<MemoryRouter>
-				<MockedProvider mocks={mocks} addTypename={false}>
-					<Home />
-				</MockedProvider>
-			</MemoryRouter>
-		);
-
-		const errorElement = await screen.findByText('Error : Failed to fetch data');
-
-		expect(errorElement).toBeInTheDocument();
-	});
-});
+})
